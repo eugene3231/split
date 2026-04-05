a69f4c2abac035cf660da1ada8d0b3a4325712ad
Eugene Chua <eugene3231@gmail.com>
2026-04-06 00:08:04 +0800

    Add react-router package

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import { ReceiptSplitterPage } from '@pages/ReceiptSplitterPage'
 import { LegacyReceiptSplitterPage } from '@pages/LegacyReceiptSplitterPage'
 
